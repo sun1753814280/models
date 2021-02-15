176576a5f079cef8ace23775155ed4ae7b52f240
Lingcao Huang <huanglingcao@gmail.com>
2021-02-15 09:25:29 -0700

    save process id to file

--- research/deeplab/train.py
+++ research/deeplab/train.py
@@ -21,6 +21,7 @@ See model.py for more details and usage.
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+import os,sys
 import six
 import tensorflow as tf
 from tensorflow.contrib import quantize as contrib_quantize
